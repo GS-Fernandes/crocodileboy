5dec590cbfbb657cf73bb6447b27d245717b0504
Gabriel Fernandes <gbielsf@gmail.com>
2019-05-30 15:30:42 -0300

    Site do Jogo

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1559224521,
+	"version": 1559240946,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -10,8 +10,11 @@
 		"images/player-sheet0.png",
 		"images/player-sheet1.png",
 		"images/tiledbackground.png",
-		"images/plataformamóvel.png",
+		"images/plataformamovel.png",
 		"images/portita-sheet0.png",
+		"images/inimigo-sheet0.png",
+		"images/limitedoimimigo-sheet0.png",
+		"images/solo.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
